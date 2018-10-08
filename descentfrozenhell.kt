8c295ac865bbd1a13ec4f85f53a15e79bd62c13d
Joseph Marsden <josephmarsden@towerdevs.xyz>
2018-10-08 13:48:31 +0100

    syntax fixes

--- descentfrozenhell.kt
+++ descentfrozenhell.kt
@@ -35,107 +35,107 @@ fun main(args: Array<String>) = withDefaultMain(
         ),
 
         root = rootEntry(CurseProvider) {
-            releaseTypes = setOf(FileType.RELEASE, FileType.BETA, FileType.ALPHA),
-            validMcVersions = setOf("1.12.2", "1.12"),
-            metaUrl = "https://curse.nikky.moe/api",
-            optionals = false,
+            releaseTypes = setOf(FileType.RELEASE, FileType.BETA, FileType.ALPHA);
+            validMcVersions = setOf("1.12.2", "1.12");
+            metaUrl = "https://curse.nikky.moe/api";
+            optionals = false;
             list {
                 // The way these mods are parsed, the Curse URL is taken,
                 // hyphens are removed and made camel case.
 
                 // InsomniaKitten
-                id(Mod.wearableBackpacks),
+                id(Mod.wearableBackpacks);
 
                 // Glitchfiend
-                id(Mod.toughAsNails),
+                id(Mod.toughAsNails);
 
                 // Beeto
-                id(Mod.betterWithMods),
+                id(Mod.betterWithMods);
                 // primetoxins
-                id(Mod.betterWithEngineering),
+                id(Mod.betterWithEngineering);
 
                 // shadowfactsmc
-                id(Mod.shadowfactsForgelin),
+                id(Mod.shadowfactsForgelin);
 
                 // BluSunrize
-                id(Mod.immersiveEngineering),
+                id(Mod.immersiveEngineering);
                 // Flaxbeard
-                id(Mod.immersivePetroleum),
+                id(Mod.immersivePetroleum);
                 // FerroO2000
-                id(Mod.immersiveTech),
+                id(Mod.immersiveTech);
 
                 // Mr_TJP
-                id(Mod.mrTJPCore),
-                id(Mod.projectRedBase),
-                id(Mod.projectRedCompat),
-                id(Mod.projectRedFabrication),
-                id(Mod.projectRedIntegration),
-                id(Mod.projectRedLighting),
+                id(Mod.mrtjpcore);
+                id(Mod.projectRedBase);
+                id(Mod.projectRedCompat);
+                id(Mod.projectRedFabrication);
+                id(Mod.projectRedIntegration);
+                id(Mod.projectRedLighting);
 
                 // chicken_bones
-                id(Mod.codeChickenLib18),
-                id(Mod.forgeMultiPartCBE),
+                id(Mod.codechickenLib18);
+                id(Mod.forgeMultipartCbe);
 
                 // Vazkii
-                id(Mod.autoRegLib),
+                id(Mod.autoreglib);
 
                 // chiselTeam
-                id(Mod.chisel),
+                id(Mod.chisel);
                 // AlgorithmX2
-                id(Mod.chiselsBits),
+                id(Mod.chiselsBits);
 
                 // jaredlll08
-                id(Mod.mtLib),
-                id(Mod.craftTweaker),
-                id(Mod.modTweaker),
+                id(Mod.mtlib);
+                id(Mod.crafttweaker);
+                id(Mod.modtweaker);
 
                 // mezz
-                id(Mod.jei),
+                id(Mod.jei);
 
                 // Team CoFH
-                id(Mod.cofhCore),
-                id(Mod.cofhWorld),
-                id(Mod.redstoneFlux),
-                id(Mod.thermalDynamics),
-                id(Mod.thermalExpansion),
+                id(Mod.cofhcore);
+                id(Mod.cofhWorld);
+                id(Mod.redstoneFlux);
+                id(Mod.thermalDynamics);
+                id(Mod.thermalexpansion);
                 
                 // azanor
-                id(Mod.thaumcraft),
-                id(Mod.thaumicComputers),
-                id(Mod.baubles),
+                id(Mod.thaumcraft);
+                id(Mod.thaumicComputers);
+                id(Mod.baubles);
                 
                 // p455w0rd
-                id(Mod.p455w0rdsLibrary),
-                id(Mod.toughExpansion),
+                id(Mod.p455w0rdsLibrary);
+                id(Mod.toughExpansion);
 
                 // asie
                 // Charset Modules
-                id(Mod.charsetLib),
-                id(Mod.charsetTweaks),
-                id(Mod.charsetImmersion),
-                id(Mod.charsetAudio),
-                id(Mod.charsetTablet),
-                id(Mod.charsetTools),
-                id(Mod.charsetBlockCarrying),
-                id(Mod.charsetPatches),
-                id(Mod.charsetStorageLocks),
+                id(Mod.charsetLib);
+                id(Mod.charsetTweaks);
+                id(Mod.charsetImmersion);
+                id(Mod.charsetAudio);
+                id(Mod.charsetTablet);
+                id(Mod.charsetTools);
+                id(Mod.charsetBlockCarrying);
+                id(Mod.charsetpatches);
+                id(Mod.charsetStorageLocks);
 
                 // Other
-                id(Mod.foamFixForMinecraft),
-                id(Mod.openComputers),
-                id(Mod.zettaIndustries),
+                id(Mod.foamfixForMinecraft);
+                id(Mod.opencomputers);
+                id(Mod.zettaIndustries);
 
                 // TeamDmfMM
-                id(Mod.catwalks4),
+                id(Mod.catwalks4);
 
                 // ondrejduchon
-                id(Mod.treeChopper),
+                id(Mod.treeChopper);
 
                 // malte0811
-                id(Mod.industrialWires),
+                id(Mod.industrialWires);
 
                 // PC-Logix
-                id(Mod.openFM),
+                id(Mod.openfm);
 
                 // Direct download (URL)
                 withProvider(DirectProvider).list {
@@ -147,75 +147,72 @@ fun main(args: Array<String>) = withDefaultMain(
                 group {
                     side = Side.SERVER
                 }.list {
-                    id("btfu-continuous-rsync-incremental-backup"),
-                    id("swingthroughgrass"),
+                    id("btfu-continuous-rsync-incremental-backup");
+                    id("swingthroughgrass");
                 }
 
                 // Specific-side mods
 
                 // Both (optional)
                 group {
-                    side = Side.BOTH,
+                    side = Side.BOTH;
                     feature {
-                        selected = false
+                        selected = false;
                     }
                 }.list {
                     id(Mod.laggoggles) {
-                        description =
-                            "***Admin/diagnostic tool. Leave off unless asked to help test performance issues."
+                        description = "***Admin/diagnostic tool. Leave off unless asked to help test performance issues.";
                     }
 
                     id(Mod.sampler) {
-                        description =
-                            "***Admin/diagnostic tool. Leave off unless asked to help test performance issues."
+                        description = "***Admin/diagnostic tool. Leave off unless asked to help test performance issues.";
                     }
 
                     id(Mod.openeye) {
-                        description =
-                            "Automatically collects and submits crash reports. Enable if asked or wish to help sort issues with the pack."
+                        description = "Automatically collects and submits crash reports. Enable if asked or wish to help sort issues with the pack.";
                     }
                 }
 
                 // Client
                 group {
-                    side = Side.CLIENT,
+                    side = Side.CLIENT;
                 }.list {
-                    id(Mod.journeyMap),
-                    id(Mod.thaumicJei),
-                    id(Mod.justEnoughResources),
-                    id(Mod.inventoryTweaks),
-                    id(Mod.betterAdvancements),
+                    id(Mod.journeymap);
+                    id(Mod.thaumicJei);
+                    id(Mod.justEnoughResourcesJer);
+                    id(Mod.inventoryTweaks);
+                    id(Mod.betterAdvancements);
 
                     group {
                         feature {
-                            selected = true,
-                            recommendation = Recommendation.starred,
+                            selected = true;
+                            recommendation = Recommendation.starred;
                         }
                     }.list {
                         id(Mod.dynamicSurroundings) {
-                            description = "Caution: Resource heavy. Quite nice, has a lot of configurable features that add immersive sound/visual effects. Includes light-level overlay. (Defaults set to remove some sounds and generally be better.)",
+                            description = "Caution: Resource heavy. Quite nice, has a lot of configurable features that add immersive sound/visual effects. Includes light-level overlay. (Defaults set to remove some sounds and generally be better.)";
                         }
                         id(Mod.dynamicLights) {
-                            description = "Caution: Resource heavy. Turn this off if your computer isn't powerful enough.",
+                            description = "Caution: Resource heavy. Turn this off if your computer isn't powerful enough.";
                         }
                     }
 
                     group {
                         feature {
-                            selected = false,
+                            selected = false;
                         }
                     }.list {
                         id(Mod.betterFoliage) {
-                            description = "Caution: Resource heavy. Disabled by default because there are a few render bugs.",
+                            description = "Caution: Resource heavy. Disabled by default because there are a few render bugs.";
                         }
                         id(Mod.fancyBlockParticles) {
-                            description = "Caution: Resource heavy. Adds some flair to particle effects and animations. Highly configurable, costs fps. (Defaults set to be less intrusive.)",
+                            description = "Caution: Resource heavy. Adds some flair to particle effects and animations. Highly configurable, costs fps. (Defaults set to be less intrusive.)";
                         }
                         id(Mod.fasterLadderClimbing) {
-                            description = "Helps you control ladder climb speed and allows you to go a bit faster.",
+                            description = "Helps you control ladder climb speed and allows you to go a bit faster.";
                         }
-                        id(Mod.nbtEdit) {
-                            description = "Can be used to view NBT trees. Developer use only.",
+                        id(Mod.nbtedit) {
+                            description = "Can be used to view NBT trees. Developer use only.";
                         }
                     }
                 }
