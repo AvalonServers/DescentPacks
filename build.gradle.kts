1a3f816c7e63f96954f2be2b33143ff45d965a81
nikky <niklasbloedorn@gmx.de>
2019-02-18 17:26:18 +0100

    update pack to 0.4.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,34 @@
 plugins {
     wrapper
-    id("voodoo") version "0.4.5-SNAPSHOT"
-    kotlin("jvm") version "1.3.20"
+    id("voodoo") version "0.4.6-SNAPSHOT"
 }
 
 voodoo {
     addTask(name = "test", parameters = listOf("test mmc"))
     addTask(name = "build", parameters = listOf("build"))
     addTask(name = "buildAndTest", parameters = listOf("build", "test mmc"))
-    addTask(name = "server", parameters = listOf("pack server"))
-    addTask(name = "buildAndPackAll", parameters = listOf("build", "pack server", "pack sk", "pack mmc"))
+    addTask(name = "packServer", parameters = listOf("pack server"))
+    addTask(name = "buildAndPackAll", parameters = listOf("build", "pack server", "pack mmc"))
 }
 
 repositories {
     maven(url = "http://maven.modmuss50.me/") {
         name = "modmuss50"
     }
-
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap") {
-        name = "Kotlin EAP"
-    }
-
     maven(url = "https://kotlin.bintray.com/kotlinx") {
         name = "kotlinx"
     }
-
-    maven(url = "https://jitpack.io") {
-        name = "jitpack"
-    }
-
     mavenCentral()
     jcenter()
+}
 
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.2"
+    distributionType = Wrapper.DistributionType.ALL
 }
 
 dependencies {
-    implementation(group = "moe.nikky.voodoo", name = "dsl", version = "0.4.5+")
-    implementation(group = "moe.nikky.voodoo", name = "voodoo", version = "0.4.5+")
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "5.1"
-    distributionType = Wrapper.DistributionType.ALL
+    implementation(group = "moe.nikky.voodoo", name = "dsl", version = "0.4.6+")
+    implementation(group = "moe.nikky.voodoo", name = "voodoo", version = "0.4.6+")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,10 @@
 pluginManagement {
     repositories {
         maven(url = "http://maven.modmuss50.me/") { name = "modmuss50" }
-        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap/") { name = "Kotlin EAP" }
-        maven(url = "https://kotlin.bintray.com/kotlinx") { name = "kotlinx" }
+        maven(url = "https://kotlin.bintray.com/kotlinx") { name = "kotlinX" }
         maven(url = "https://jitpack.io") { name = "jitpack" }
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
-rootProject.name = "DescentFrozenHell"
+rootProject.name = "DescentPacks"
