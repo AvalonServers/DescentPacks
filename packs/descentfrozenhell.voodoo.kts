4ae53a27d24a70352d76c5493e8cd6d8c24bb677
Skye M <skyem@hotmail.co.uk>
2019-05-17 19:26:53 +0100

    Version bump

--- packs/descentfrozenhell.voodoo.kts
+++ packs/descentfrozenhell.voodoo.kts
@@ -13,7 +13,7 @@ import voodoo.provider.DirectProvider
 mcVersion = "1.12.2"
 title = "Descent Frozen Hell"
 authors = listOf("CitadelCore", "Skye")
-version = "1.0.7.1"
+version = "1.1-alpha.0"
 
 forge = Forge.mc1_12_2_latest
 icon = rootDir.resolve("icon.png")
