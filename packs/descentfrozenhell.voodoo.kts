f8d40531c0932eff3c9dcc63c62b67532ef1a20e
Skye M <skyem@hotmail.co.uk>
2019-07-15 16:33:55 +0100

    Remove fuel requirement from Railcraft Worldspikes Closes issue #41

--- packs/descentfrozenhell.voodoo.kts
+++ packs/descentfrozenhell.voodoo.kts
@@ -12,7 +12,7 @@ import voodoo.provider.DirectProvider
 mcVersion = "1.12.2"
 title = "Descent Frozen Hell"
 authors = listOf("CitadelCore", "Skye")
-version = "1.1.2"
+version = "1.1.2.1"
 
 forge = Forge.mc1_12_2_latest
 icon = rootDir.resolve("icon.png")
