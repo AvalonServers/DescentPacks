1910129d9b67818b99455635008e4c623d1d9286
Joseph Marsden <CitadelCore@users.noreply.github.com>
2019-08-06 18:35:18 +0100

    Fix voodoo and update pack (#63)
    * Fix broken Voodoo
    * Update pack mods

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     repositories {
-        maven(url = "http://maven.modmuss50.me/") { name = "modmuss50" }
+        maven(url = "https://maven.modmuss50.me/") { name = "modmuss50" }
         maven(url = "https://kotlin.bintray.com/kotlinx") { name = "kotlinX" }
         maven(url = "https://jitpack.io") { name = "jitpack" }
         mavenCentral()
