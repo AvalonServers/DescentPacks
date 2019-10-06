2ff56faf487712b3e24dec930a6de0024a43cb11
Skye M <skyem@hotmail.co.uk>
2019-10-06 16:14:42 +0100

    Remove jitpack

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ pluginManagement {
     repositories {
         maven(url = "https://maven.modmuss50.me/") { name = "modmuss50" }
         maven(url = "https://kotlin.bintray.com/kotlinx") { name = "kotlinX" }
-        maven(url = "https://jitpack.io") { name = "jitpack" }
         mavenCentral()
         gradlePluginPortal()
     }
