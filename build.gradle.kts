8f33546d1bd4e4b5055f79da94710c4b5c7860ef
Skye <skyem123@users.noreply.github.com>
2019-10-11 15:00:28 +0100

    Remove jitpack (#75)
    * Remove jitpack
    
    * Build and update pack
    
    * Update meta zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,11 +24,7 @@ repositories {
     maven(url = "https://kotlin.bintray.com/kotlinx") {
         name = "kotlinx"
     }
-
-    maven(url = "https://jitpack.io") {
-        name = "jitpack"
-    }
-
+    
     mavenCentral()
     jcenter()
 
