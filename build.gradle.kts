37423258f3b09605516a03922c1a7dc2f43d9fad
nikky <root@nikky.moe>
2020-09-03 03:31:07 +0200

    update voodoo to fix descent
    sorry for the large amount of diff in .lockfiles

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     wrapper
-    id("voodoo") version "0.5.0-SNAPSHOT"
+    id("voodoo") version "0.5.20-SNAPSHOT"
 }
 
 voodoo {
@@ -20,50 +20,22 @@ voodoo {
     }
     addTask("packAll") {
         pack().server()
-        pack().experimental()
-        pack().multimcExperimental()
+        pack().voodoo()
+        pack().multimcVoodoo()
     }
     addTask("buildAndPackAll") {
         build()
         pack().server()
-        pack().experimental()
-        pack().multimcExperimental()
+        pack().voodoo()
+        pack().multimcVoodoo()
     }
-    addTask(name = "pack_experimental") {
-        pack().experimental()
+    addTask(name = "pack_voodoo") {
+        pack().voodoo()
     }
 
     generateCurseforgeMods("Mod", "1.12", "1.12.1", "1.12.2")
     generateCurseforgeTexturepacks("TexturePack", "1.12", "1.12.1", "1.12.2")
     generateForge("Forge_12_2", "1.12.2")
-}
-
-repositories {
-    mavenLocal()
-
-    maven(url = "https://maven.modmuss50.me/") {
-        name = "modmuss50"
-    }
-
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap") {
-        name = "Kotlin EAP"
-    }
-
-    maven(url = "https://kotlin.bintray.com/kotlinx") {
-        name = "kotlinx"
-    }
-    
-    mavenCentral()
-    jcenter()
-}
-
-dependencies {
-    kotlinScriptDef(group = "moe.nikky.voodoo", name = "voodoo", version = "0.5.0+")
-    kotlinScriptDef(group = "moe.nikky.voodoo", name = "dsl", version = "0.5.0+")
-    implementation(group = "moe.nikky.voodoo", name = "voodoo", version = "0.5.0+")
-    implementation(group = "moe.nikky.voodoo", name = "dsl", version = "0.5.0+")
-//    implementation(kotlin("stdlib-jdk8"))
-//    implementation(kotlin("script-runtime"))
 }
 
 tasks.withType<Wrapper> {
